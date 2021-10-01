e46459fb3dd3968afee31959285bd0f0fbc89084
evgen <57286862+evgen-chern@users.noreply.github.com>
2021-10-01 16:18:19 +0300

    releaze 2.0

--- js/script.js
+++ js/script.js
@@ -102,4 +102,43 @@ window.onload = function(){
         ]
     })
 
+    const animItems = document.querySelectorAll('.anim_item');
+    if (animItems.length >0){
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll(){
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+                if(animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('anim');
+                } else {
+                    if(!animItem.classList.contains('no_anim')){
+                        animItem.classList.remove('anim');
+                    }
+                }
+            }
+        }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+
+        setTimeout(() => {
+            animOnScroll();
+        }, 300);
+
+    }
+
+
 }
